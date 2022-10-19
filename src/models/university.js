63be4bc843673d4cc8810f63c47c63ac0509036e
Edson Cavalcante de Albuquerque Bisneto <51378539+edsonkz@users.noreply.github.com>
2022-10-19 11:14:55 -0300

    Script para popular o banco de dados terminado.

--- src/models/university.js
+++ src/models/university.js
@@ -2,13 +2,17 @@ import { model, Schema } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
 const UniversitySchema = new Schema({
-    nome: { type: String, required: true },
-    country: { type: String, required: true },
-    state_province: { type: String, required: false },
-    alpha_two_codes: { type: String, required: true, maxLength: 2, minLength: 2 },
-    domains: [{ _id: false, type: String, required: false }],
-    web_pages: [{ _id: false, type: String, required: false }],
-
+	name: { type: String, required: true },
+	country: { type: String, required: true },
+	"state-province": { type: String, required: false },
+	alpha_two_code: {
+		type: String,
+		required: true,
+		maxLength: 2,
+		minLength: 2,
+	},
+	domains: [{ _id: false, type: String, required: false }],
+	web_pages: [{ _id: false, type: String, required: false }],
 });
 
 UniversitySchema.plugin(mongoosePaginate);
--- src/populateDB.js
+++ src/populateDB.js
@@ -3,35 +3,49 @@ import { url, mongo, connectionParams } from "./database/db";
 import { University } from "./models/university";
 
 mongo
-    .connect(url, connectionParams)
-    .then(async () => {
-        console.log("Connected to database ");
-        await main();
-        mongo.connection.close();
-    })
-    .catch((err) => {
-        console.error(`Error connecting to the database. \n${err}`);
-    });
+	.connect(url, connectionParams)
+	.then(async () => {
+		console.log("Connected to database ");
+		await main();
+		mongo.connection.close();
+	})
+	.catch((err) => {
+		console.error(`Error connecting to the database. \n${err}`);
+	});
 
 async function create(universities) {
-    let newUniversity = new University(universities);
-    newUniversity.save(universities);
+	for (let university of universities) {
+		console.log(university);
+		let newUniversity = new University(university);
+		await newUniversity.save(university);
+	}
 }
 
 async function main() {
-    try {
-        let countries = ["brazil, argentina, chile, colombia, paraguay, peru, suriname, uruguay"];
-        let response;
+	try {
+		let countries = [
+			"brazil",
+			"argentina",
+			"chile",
+			"colombia",
+			"paraguay",
+			"peru",
+			"suriname",
+			"uruguay",
+		];
+		let response;
 
-        for (let country of countries){
-        response = (await api.get("search", { params: { country} })).data;
+		for (const country of countries) {
+			console.log(country);
+			response = (
+				await api.get("search", { params: { country: country } })
+			).data;
 
-        console.log(response);
-        await create(response);
-        }
-
-    } catch (error) {
-        console.log(error.message);
-        return true;
-    }
+			//console.log(response);
+			await create(response);
+		}
+	} catch (error) {
+		console.log(error.message);
+		return true;
+	}
 }
